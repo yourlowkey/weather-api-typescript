70861192a7c3b06902bd53d14bd0e99564ae3681
yourlowkey <m.phuc2552000@gmail.com>
2023-06-27 22:14:07 +0700

    weather api

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,13 @@
+import { useState } from 'react'
+
 const App = () => {
+  const [term, setTerm] = useState('')
+
+  const onInputChange =(e:any)=>{
+    console.log(e.target.value)
+  }
+  //http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}
+
   return (
     <main className="flex justify-center items-center bg-gradient-to-br from-sky-400 via-rose-400 to-lime-400 h-[100vh] w-full">
       <section className="w-full md:max-w-[500px] p-4 flex flex-col text-center items-center justify-center md:px-10 lg:p-24 h-full lg:h-[500px] bg-white bg-opacity-20 backdrop-blur-ls rounded drop-shadow-lg text-zinc-700">
@@ -11,19 +20,19 @@ const App = () => {
           from dropdown
         </p>
         <div className="relative flex mt-10 md:mt-4">
-      <input
-        type="text"
-        value={''}
-        className="px-2 py-1 rounded-l-md border-2 border-white"
-        // onChange={onInputChange}
-      />
-      <button
-        className="rounded-r-md border-2 border-zinc-100 hover:border-zinc-500 hover:text-zinc-500  text-zinc-100 px-2 py-1 cursor-pointer"
-        // onClick={onSubmit}
-      >
-        search
-      </button>
-    </div>
+          <input
+            type="text"
+            value={term}
+            className="px-2 py-1 rounded-l-md border-2 border-white"
+          onChange={onInputChange}
+          />
+          <button
+            className="rounded-r-md border-2 border-zinc-100 hover:border-zinc-500 hover:text-zinc-500  text-zinc-100 px-2 py-1 cursor-pointer"
+          // onClick={onSubmit}
+          >
+            search
+          </button>
+        </div>
       </section>
     </main>
   )
